98c7638bf8f718ca1190d162976ea4cc3437246a
Mahadevan Reji <rmahadevan574@gmail.com>
2026-03-21 22:01:24 +0530

    feat: log prerendering duration for server load and individual route generation.

--- prerender.ts
+++ prerender.ts
@@ -8,14 +8,23 @@ const toAbsolute = (p: string) => path.resolve(__dirname, p);
 const routesToPrerender: string[] = ["/", "/pdf"];
 
 async function prerender() {
+  const nowStart = new Date() as any;
+
   const template = fs.readFileSync(
     toAbsolute("dist/static/index.html"),
     "utf-8",
   );
 
+  const start = new Date() as any;
   const { render } = await import(toAbsolute("dist/server/entry-server.js"));
 
+  const endStart = new Date() as any;
+
+  console.log(`Server loaded in ${endStart - start}ms`);
+
   for (const route of routesToPrerender) {
+    const now = new Date() as any;
+
     const { body, links } = render(route);
     const html = template
       .replace("<!--app-->", body)
@@ -26,7 +35,13 @@ async function prerender() {
       `dist/static${route === "/" ? "/index" : route}.html`,
     );
     fs.writeFileSync(filePath, html);
+
+    const end = new Date() as any;
+    console.log(`Route ${route} generated in ${end - now}ms`);
   }
+
+  const nowEnd = new Date() as any;
+  console.log("\x1b[32m%s\x1b[0m", `Total time taken: ${nowEnd - nowStart}ms`);
 }
 
 prerender()
